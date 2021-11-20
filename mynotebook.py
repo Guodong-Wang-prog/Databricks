3b64e37022052fdc68243bb0f3e871bc93a1ebd0
Guodong-Wang-prog <60081730+Guodong-Wang-prog@users.noreply.github.com>
2021-11-20 18:09:43 +0800

    Cluster init scripts trying

--- mynotebook.py
+++ mynotebook.py
@@ -221,3 +221,117 @@ df.head(5)
 # COMMAND ----------
 
 # MAGIC %fs ls /mnt/mounttest/
+
+# COMMAND ----------
+
+# MAGIC %md #adls-gen2-access-key
+
+# COMMAND ----------
+
+#Configure a storage account access key for authentication
+spark.conf.set(
+  "fs.azure.account.key.darabrickslake.dfs.core.windows.net", 
+  dbutils.secrets.get(scope="keyvault-secrets", key="example-adls2-secret"))
+
+# COMMAND ----------
+
+#Create a storage container
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
+dbutils.fs.ls("abfss://createdby-databricksnotebook@darabrickslake.dfs.core.windows.net/")
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
+
+# COMMAND ----------
+
+#Read Databricks Dataset IoT Devices JSON
+df = spark.read.json("dbfs:/databricks-datasets/iot/iot_devices.json")
+
+# COMMAND ----------
+
+#Write IoT Devices JSON
+df.write.json("abfss://createdby-databricksnotebook@darabrickslake.dfs.core.windows.net/iot_devices.json")
+
+# COMMAND ----------
+
+#List filesystem
+dbutils.fs.ls("abfss://createdby-databricksnotebook@darabrickslake.dfs.core.windows.net/")
+
+# COMMAND ----------
+
+#Read IoT Devices JSON from ADLS Gen2 filesystem
+df2 = spark.read.json("abfss://createdby-databricksnotebook@darabrickslake.dfs.core.windows.net/iot_devices.json")
+display(df2)
+
+# COMMAND ----------
+
+# MAGIC %md #Access ADLS using OAuth2.0
+
+# COMMAND ----------
+
+configs = {"fs.azure.account.auth.type": "OAuth",
+          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+          "fs.azure.account.oauth2.client.id": "8ca022f0-1560-4dd2-b1f1-8317eab6ab8d",
+          "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="keyvault-secrets",key="client-secret"),
+          "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token"}
+
+# Optionally, you can add <directory-name> to the source URI of your mount point.
+dbutils.fs.mount(
+  source = "abfss://createdby-databricksnotebook@darabrickslake.dfs.core.windows.net/",
+  mount_point = "/mnt/adlsoauth",
+  extra_configs = configs)
+
+# COMMAND ----------
+
+df2 = spark.read.json("/mnt/adlsoauth/iot_devices.json")
+display(df2)
+
+# COMMAND ----------
+
+dbutils.fs.ls("/mnt/adlsoauth")
+
+# COMMAND ----------
+
+# MAGIC %md #Drive node local file system trying
+
+# COMMAND ----------
+
+# MAGIC %sh ls logs
+
+# COMMAND ----------
+
+# MAGIC %sh cat logs/stderr
+
+# COMMAND ----------
+
+# MAGIC %sh ls /databricks/python/bin
+
+# COMMAND ----------
+
+# MAGIC %md #Cluster init scripts
+
+# COMMAND ----------
+
+#Create a DBFS directory you want to store the init script in. This example uses dbfs:/databricks/scripts.
+dbutils.fs.mkdirs("dbfs:/databricks/scripts/")
+
+# COMMAND ----------
+
+#Create a script named postgresql-install.sh in that directory:
+dbutils.fs.put("/databricks/scripts/postgresql-install.sh","""
+#!/bin/bash
+wget --quiet -O /mnt/driver-daemon/jars/postgresql-42.2.2.jar https://repo1.maven.org/maven2/org/postgresql/postgresql/42.2.2/postgresql-42.2.2.jar""", True)
+
+# COMMAND ----------
+
+#Check that the script exists.
+display(dbutils.fs.ls("dbfs:/databricks/scripts/postgresql-install.sh"))
+
+# COMMAND ----------
+
+# MAGIC %fs ls cluster-logs/1111-133045-rx27q9ov/init_scripts/1111-133045-rx27q9ov_10_139_64_6/
+
+# COMMAND ----------
+
+# read the file
+with open("/dbfs/cluster-logs/1111-133045-rx27q9ov/init_scripts/1111-133045-rx27q9ov_10_139_64_6/20211120_092728_00_postgresql-install.sh.stderr.log", "r") as f_read:
+  for line in f_read:
+    print(line)
